40ce97dfc52873a8f3a9b0f7e55073fd1eb3db87
mohkhoirularifin <karifin998@gmail.com>
2019-12-25 13:47:22 +0700

    Membuat MusaActivity

--- app/src/main/java/com/example/uasmobile/nabi/IbrahimActivity.java
+++ app/src/main/java/com/example/uasmobile/nabi/IbrahimActivity.java
@@ -1,8 +1,10 @@
-package com.example.uasmobile.fragments;
+package com.example.uasmobile.nabi;
+
+import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.example.uasmobile.R;
 
 public class IbrahimActivity extends AppCompatActivity {
 
---
+++ app/src/main/java/com/example/uasmobile/nabi/MusaActivity.java
@@ -0,0 +1,16 @@
+package com.example.uasmobile.nabi;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.uasmobile.R;
+
+public class MusaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_musa);
+    }
+}
