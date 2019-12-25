18adc4709491261bb82dc4a93f224d57d4cde5fa
mohkhoirularifin <karifin998@gmail.com>
2019-12-25 13:46:08 +0700

    Membuat IbrahimActivity

---
+++ app/src/main/java/com/example/uasmobile/fragments/IbrahimActivity.java
@@ -0,0 +1,14 @@
+package com.example.uasmobile.fragments;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class IbrahimActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ibrahim);
+    }
+}
