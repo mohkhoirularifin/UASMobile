baaaabb71c3dc5fcb9d39aeafaaaff803e5f4ba1
mohkhoirularifin <karifin998@gmail.com>
2019-12-26 02:11:46 +0700

    Edit MeFragment & Selesai

--- app/src/main/java/com/example/uasmobile/fragments/MeFragment.java
+++ app/src/main/java/com/example/uasmobile/fragments/MeFragment.java
@@ -1,10 +1,13 @@
 package com.example.uasmobile.fragments;
 
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 
 import androidx.fragment.app.Fragment;
 
@@ -14,7 +17,7 @@ import com.example.uasmobile.R;
  * A simple {@link Fragment} subclass.
  */
 public class MeFragment extends Fragment {
-
+    private View v;
 
     public MeFragment() {
         // Required empty public constructor
@@ -25,7 +28,53 @@ public class MeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_me, container, false);
+        v = inflater.inflate(R.layout.fragment_me,container, false);
+        addListenerOnButton();
+        return v;
+    }
+
+    public void addListenerOnButton() {
+
+        ImageButton ib1 = (ImageButton) v.findViewById(R.id.fb);
+        ImageButton ib2 = (ImageButton) v.findViewById(R.id.tw);
+        ImageButton ib3 = (ImageButton) v.findViewById(R.id.ig);
+
+        ib1.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Intent int1 = new Intent();
+                int1.setAction(Intent.ACTION_VIEW);
+                int1.addCategory(Intent.CATEGORY_BROWSABLE);
+                int1.setData(Uri.parse("https://www.facebook.com/khoierulz.chpandean"));
+                startActivity(int1);
+            }
+
+        });
+        ib2.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Intent int2 = new Intent();
+                int2.setAction(Intent.ACTION_VIEW);
+                int2.addCategory(Intent.CATEGORY_BROWSABLE);
+                int2.setData(Uri.parse("https://twitter.com/moh_khoirul_a_"));
+                startActivity(int2);
+            }
+
+        });
+        ib3.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Intent int3 = new Intent();
+                int3.setAction(Intent.ACTION_VIEW);
+                int3.addCategory(Intent.CATEGORY_BROWSABLE);
+                int3.setData(Uri.parse("https://www.instagram.com/moh.khoirularifin_/"));
+                startActivity(int3);
+            }
+
+        });
     }
 
 }
